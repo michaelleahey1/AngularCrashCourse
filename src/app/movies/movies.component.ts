9fb9cb01e96d2feb578156ed6910e4f7e7ed57d9
Michael Leahey <michael.leahey1843@outlook.com>
2022-06-15 17:09:45 -0400

    update

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -13,19 +13,26 @@ export class MoviesComponent implements OnInit {
   ngOnInit(): void {
   }
 movie: movie[]=[
-  {title: 'Casablanca', Cast: 'Humphrey Bogart, Ingred Begman', Release_Date: 1942, Director: 'Michael Curtiz'},
-  {title: 'The Man Who Knew Too Much', Cast: 'James Stewart, Doris Day', Release_Date: 1956, Director: 'Alfred Hitchcock'},
-  {title: 'Frankenstein', Cast: 'Colin Clive, Boris Karlov', Release_Date: 1931, Director: 'James Whale'},
-  {title: 'Psycho', Cast: 'Anthony Perkins, Janet Leigh', Release_Date: 1960, Director: 'Alfred Hitchcock'},
-  {title: 'All Quiet on the Western Front', Cast: 'Lew Ayres, Loius Wolheim', Release_Date: 1930, Director: 'Lewis Milestone'},
-  {title: 'To Kill a Mockingbird', Cast: 'Gregory Peck, Mary Badham', Release_Date: 1962, Director: 'Robert Mulligan'},
-  {title: "Twelve O'Clock High", Cast: 'Gregory Peck, Hugh Marlowe', Release_Date: 1949, Director: 'Henry King'},
-  {title:'The Day The Earth Stood Still', Cast: 'Michael Rennie, Patricia Neal', Release_Date: 1951, Director: 'Robert Wise'}
+  {title: 'Casablanca', Cast: 'Humphrey Bogart, Ingred Begman', Release_Date: 1942, Director: 'Michael Curtiz',display:false },
+  {title: 'The Man Who Knew Too Much', Cast: 'James Stewart, Doris Day', Release_Date: 1956, Director: 'Alfred Hitchcock',display:false},
+  {title: 'Frankenstein', Cast: 'Colin Clive, Boris Karlov', Release_Date: 1931, Director: 'James Whale',display:false},
+  {title: 'Psycho', Cast: 'Anthony Perkins, Janet Leigh', Release_Date: 1960, Director: 'Alfred Hitchcock',display:false},
+  {title: 'All Quiet on the Western Front', Cast: 'Lew Ayres, Loius Wolheim', Release_Date: 1930, Director: 'Lewis Milestone',display:false},
+  {title: 'To Kill a Mockingbird', Cast: 'Gregory Peck, Mary Badham', Release_Date: 1962, Director: 'Robert Mulligan',display:false},
+  {title: "Twelve O'Clock High", Cast: 'Gregory Peck, Hugh Marlowe', Release_Date: 1949, Director: 'Henry King',display:false},
+  {title:'The Day The Earth Stood Still', Cast: 'Michael Rennie, Patricia Neal', Release_Date: 1951, Director: 'Robert Wise',display:false}
 ]
+  displayMovie(movieNumber:number){
+    for (let i = 0; i < movie.length; i++){
+      this.movie[i].display= false;
+    }
+      this.movie[movieNumber].display = true
+  }
 }
 class movie{
   title: string | undefined;
   Cast:string | undefined;
   Release_Date: number | undefined;
   Director:string | undefined;
+  display:boolean | undefined;
 }
