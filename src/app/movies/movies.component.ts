a58e923da1883bee859b38ca5ecc808ab844d507
Michael Leahey <michael.leahey1843@outlook.com>
2022-06-15 11:49:07 -0400

    update

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movies',
-  templateUrl: './movies.component.html',
+  templateUrl: './Custom.html',
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
@@ -14,7 +14,7 @@ export class MoviesComponent implements OnInit {
 movie: movie[]=[
   {title: 'Casablanca', Cast: 'Humphrey Bogart, Ingred Begman', Release_Date: 1942, Director: 'Michael Curtiz'},
   {title: 'The Man Who Knew Too Much', Cast: 'James Stewart, Doris Day', Release_Date: 1956, Director: 'Alfred Hitchcock'},
-  {title: 'Frankenstein', Cast: 'Colin Clive, Boris Karlov', Release_Date: 1931, Director: 'James Whale'},
+  {title: 'Frankenstein', Cast: 'Colon Clive, Boris Karlov', Release_Date: 1931, Director: 'James Whale'},
   {title: 'Psycho', Cast: 'Anthony Perkins, Janet Leigh', Release_Date: 1960, Director: 'Alfred Hitchcock'}
 ]
 }
